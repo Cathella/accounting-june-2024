9adaf23700dd09e9a33dd95b65c00ad432ab3c51
cathella <ncathie214@gmail.com>
2024-06-02 16:47:19 +0300

    Refactor ReportsNav component

--- src/components/ReportsNav.js
+++ src/components/ReportsNav.js
@@ -1,20 +1,26 @@
-function ReportsNav() {
-  return (
-    <div className="flex gap-5 px-6 text-base font-semibold leading-6 text-neutral-400 max-md:flex-wrap max-md:pr-5">
-      <div className="justify-center px-6 py-4 bg-white rounded-lg text-slate-500 max-md:px-5">
-        Account Statements
-      </div>
-      <div className="justify-center items-start px-6 py-4 bg-white rounded-lg max-md:px-5">
-        Balance Sheets
-      </div>
-      <div className="justify-center items-start px-6 py-4 bg-white rounded-lg max-md:px-5">
-        Trial Balance
-      </div>
-      <div className="justify-center items-start px-6 py-4 bg-white rounded-lg max-md:px-5">
-        User Activity
-      </div>
-    </div>
-  );
-}
+import * as React from "react";
+
+const Card = ({ children }) => (
+  <section className="justify-center px-6 py-4 bg-white rounded-lg max-md:px-5">
+    {children}
+  </section>
+);
+
+const ReportsNav = () => (
+  <div className="flex gap-5 pr-20 text-base font-semibold leading-6 text-neutral-400 max-md:flex-wrap max-md:pr-5">
+    <Card>
+      <span className="text-slate-500">Account Statements</span>
+    </Card>
+    <Card>
+      <span className="items-start">Balance Sheets</span>
+    </Card>
+    <Card>
+      <span className="items-start">Trial Balance</span>
+    </Card>
+    <Card>
+      <span className="items-start">User Activity</span>
+    </Card>
+  </div>
+);
 
 export default ReportsNav;
